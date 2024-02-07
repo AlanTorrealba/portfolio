f268c711d04adc7c363ba4f10b6fa9c81b20b32a
AlanT <antorrealbatapia@gmail.com>
2024-02-07 14:43:53 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Button, ButtonGroup } from "@nextui-org/react";
+import NavBar2 from "./components/NavBar2";
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <div className="bg-blue-700">
-      <div className="flex flex-row">
-        <Button className="" radius="full" color="primary">
-          Button
-        </Button>
-      </div>
+    <div className="m-10 p-10">
+      <NavBar2/>
+    
     </div>
   );
 }
